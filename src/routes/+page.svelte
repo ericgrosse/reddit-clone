cb1c0ed9f7bed97fb401ee78cadfd6f72b664f18
Eric Grosse <ericgrosse1@gmail.com>
2023-01-26 12:09:45 -0500

    Updated page.svelte to not show boilerplate message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <h1>Reddit Clone</h1>
+  <p>A reddit clone written in svelte (frontend) and backend TBD</p>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+}
+</style>
