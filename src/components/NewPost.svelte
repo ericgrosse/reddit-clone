d7292af9e01f2f1f9467287351903340ec02cffd
Eric Grosse <ericgrosse1@gmail.com>
2023-01-26 16:19:41 -0500

    Implemented localstorage for mock api post requests

--- src/components/NewPost.svelte
+++ src/components/NewPost.svelte
@@ -3,10 +3,37 @@
   let author;
   let body;
 
+  function createNewRedditPost(title, author, body) {
+    // Create a new object to hold the post data
+    let newPost = {
+      title: title,
+      author: author,
+      body: body,
+      upvotes: 0,
+      downvotes: 0,
+      comments: [],
+      date_created: new Date()
+    };
+
+    // Get the existing posts from local storage
+    let subredditPosts = JSON.parse(localStorage.getItem('subreddit')) || [];
+
+    // Add the new post to the list of posts
+    subredditPosts.push(newPost);
+
+    // Save the updated list of posts to local storage
+    localStorage.setItem('subreddit', JSON.stringify(subredditPosts));
+
+    // Return the new post object
+    return newPost;
+  }
+
   function handleClick() {
     console.log(title);
     console.log(author);
     console.log(body);
+
+    createNewRedditPost(title, author, body);
   }
 
   function handleTitle(evt) {
